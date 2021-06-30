c09796bd089612b5f48dc381b2f2feabbf7d1617
João Pedro <jpsdb10@hotmail.com>
2021-06-30 12:01:51 -0300

    implementação de hashCode e equals em classe personalizada

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,23 @@
 package application;
 
+import entities.Client;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Client c1 = new Client("Maria", "maria@gmail.com");
+		Client c2 = new Client("Maria", "maria@gmail.com");
+		
+		String s1 = "Test";
+		String s2 = "Test";
+		
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.equals(c2));
+		
+		System.out.println(c1 == c2); // O == analisa a igualdade de referência na memória
+		
+		System.out.println(s1 == s2); // O compilador trata de maneira diferente as formas literais
 	}
-
 }
